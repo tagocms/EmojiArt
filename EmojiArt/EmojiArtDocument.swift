6eb97f57359de1ef8d4729d6edd9fc6578ae0682
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2025-10-24 17:07:27 -0300

    Lecture 10

---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,45 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Tiago Camargo Maciel dos Santos on 24/10/25.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    typealias Emoji = EmojiArt.Emoji
+    typealias Position = EmojiArt.Emoji.Position
+    
+    @Published private var emojiArt = EmojiArt()
+    
+    var emojis: [Emoji] { emojiArt.emojis }
+    var background: URL? { emojiArt.background }
+    
+    init() {
+        self.emojiArt.addEmoji("🚲", at: .init(x: -200, y: -150), size: 200)
+        self.emojiArt.addEmoji("🔥", at: .init(x: 200, y: 100), size: 80)
+    }
+    
+    // MARK: - Intents
+    func setBackground(_ url: URL?) {
+        emojiArt.background = url
+    }
+    
+    func addEmoji(_ emoji: String, at position: Emoji.Position, size: CGFloat) {
+        emojiArt.addEmoji(emoji, at: position, size: Int(size))
+    }
+}
+
+extension EmojiArt.Emoji {
+    var font: Font {
+        Font.system(size: CGFloat(size))
+    }
+}
+
+extension EmojiArt.Emoji.Position {
+    func `in`(_ geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+        return CGPoint(x: center.x + CGFloat(x), y: center.y - CGFloat(y))
+    }
+}
