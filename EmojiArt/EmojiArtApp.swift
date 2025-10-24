aace6e4b803e2cc2cde035cfdff4ea19f6267044
Tiago Camargo Maciel dos Santos <tagocms@gmail.com>
2025-10-24 15:42:09 -0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Tiago Camargo Maciel dos Santos on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
